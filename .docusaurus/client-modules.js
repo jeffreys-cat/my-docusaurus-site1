e798b7c4f03356633eb636adc5ad9afa28c78a6f
colordove <color.dove@gmail.com>
2022-07-01 17:11:24 +0800

    [feature] add netlify-cms

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/hujian/Documents/Code/my-docusaurus-site1/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/hujian/Documents/Code/my-docusaurus-site1/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/hujian/Documents/Code/my-docusaurus-site1/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/hujian/Documents/Code/my-docusaurus-site1/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/hujian/Documents/Code/my-docusaurus-site1/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/hujian/Documents/Code/my-docusaurus-site1/src/css/custom.css"),
+];
